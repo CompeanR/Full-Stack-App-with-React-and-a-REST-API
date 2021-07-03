a8a220ab490b95a039871a7fe07ec7094dc3317d
compean <javier.compeanr@gmail.com>
2021-07-02 20:39:21 -0500

    failing trying to do the project with hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
-import Courses from './components/Courses';
+import Courses from './components/Courses'
 
 const App = () => (
   <Router>
     <div>
-      
+      <Switch>
+        <Route exact path="/" component={Courses} />
+      </Switch>
     </div>
   </Router>
-);
+)
 
 export default App;
+;
+  
